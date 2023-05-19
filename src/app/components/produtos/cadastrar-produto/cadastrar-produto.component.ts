6eb2e8f5842aee0ed8a0a0f195f6dfdb7b2befd5
Alexander <alexander.oliveira3@etec.sp.gov.br>
2023-05-18 22:35:30 -0300

    Aula do dia 18/05

--- src/app/components/produtos/cadastrar-produto/cadastrar-produto.component.ts
+++ src/app/components/produtos/cadastrar-produto/cadastrar-produto.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+
+
+
 @Component({
-  selector: 'app-cadastrar-produto',
-  templateUrl: './cadastrar-produto.component.html',
-  styleUrls: ['./cadastrar-produto.component.css']
+
+selector: 'app-cadastrar-produto',
+
+templateUrl: './cadastrar-produto.component.html',
+
+styleUrls: ['./cadastrar-produto.component.css']
+
 })
+
 export class CadastrarProdutoComponent {
+
+nome:string = '';
+
+validade:string = '';
+
+preco:number = 0;
+
+
+
+
+salvarProduto(): void{
+
+console.log('Nome: ', this.nome);
+console.log('Validade', this.validade);
+console.log('Preço', this.preco);
+
+alert('Salvo com sucesso!');
+}
+
+
+
 
 }
